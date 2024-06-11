fb80fe88661f9b1110b08476b7fb0538333834b7
sigmammal <luisahill456@gmail.com>
2024-06-11 16:42:46 +0100

    Getting last 50 songs
    Gets last 50 songs, still only working with manual input of auth code.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import base64
 from requests import post, get
 import json
 import urllib.parse
+import webbrowser
+
 
 load_dotenv()
 
@@ -14,20 +16,19 @@ client_secret = os.getenv("CLIENT_SECRET")
 scopes = os.getenv("SCOPES", "user-read-private user-read-email")
 redirect_uri = os.getenv("REDIRECT_URI", "http://localhost:8888/callback")
 
-auth_query_parameters = {
-    "response_type": "code",
-    "client_id": client_id,
-    "scope": scopes,
-    "redirect_uri": redirect_uri,
-    "show_dialog": "true"
-}
-auth_url = "https://accounts.spotify.com/authorize?" + urllib.parse.urlencode(auth_query_parameters)
-print("Go to the following URL to authorize the application:")
-print(auth_url)
+def get_auth_url():
+    auth_query_parameters = {
+        "response_type": "code",
+        "client_id": client_id,
+        "scope": scopes,
+        "redirect_uri": redirect_uri,
+        "show_dialog": "true"
+    }
 
+    return "https://accounts.spotify.com/authorize?" + urllib.parse.urlencode(auth_query_parameters)
 
-def get_token():
-    auth_code = input("Enter auth code: ")
+def get_token(code):
+
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
@@ -39,30 +40,42 @@ def get_token():
     }
     data = {
         "grant_type": "authorization_code",
-        "code": auth_code,
+        "code": code,
         "redirect_uri": redirect_uri
     }
     result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
+    token = json.loads(result.content)["access_token"]
     return token
-
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
 
-def get_profile_info(token):
-    url = "https://api.spotify.com/v1/me"
+def get_recent_tracks(token):
+    url = "https://api.spotify.com/v1/me/player/recently-played"
     headers = get_auth_header(token)
+    query = "?limit=50"
 
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)
+    result = get(url + query, headers=headers)
+    json_result = json.loads(result.content)["items"]
     return json_result
 
-token = get_token()
-print(token)
-profile = get_profile_info(token)
-print(profile)
+if __name__ == '__main__':
+    auth_url = get_auth_url()
+    print("Please go to this URL and authorize the application:")
+    print(auth_url)
+
+    # Open the URL in the default web browser
+    webbrowser.open(auth_url)
+
+    # Get the authorization code from the user
+    code = input("Enter the authorization code: ")
+
+    # Exchange the authorization code for an access token
+    token = get_token(code)
+
+    # Get and print the user's recently played tracks
+    recent_tracks = get_recent_tracks(token)
+    print(json.dumps(recent_tracks, indent=4))
 
 
